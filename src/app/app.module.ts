e37e67366f9bf9f18255b033b69415cc18a8b315
nextgame2024 <nextgamebrisbane@gmail.com>
2025-02-02 21:53:44 +1000

    directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { CourseCardComponent } from "./course-card/course-card.component";
 import { CourseImageComponent } from "./course-image/course-image.component";
 import { HighlightedDirective } from "./directives/highlighted.directive";
+import { NgxUnlessDirective } from "./directives/ngx-unless.directive";
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HighlightedDirective } from "./directives/highlighted.directive";
     CourseCardComponent,
     CourseImageComponent,
     HighlightedDirective,
+    NgxUnlessDirective,
   ],
   imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
--- src/app/directives/ngx-unless.directive.ts
+++ src/app/directives/ngx-unless.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, TemplateRef, ViewContainerRef } from "@angular/core";
 
 @Directive({
-  selector: '[ngxUnless]'
+  selector: "[ngxUnless]",
+  standalone: false,
 })
 export class NgxUnlessDirective {
+  visible = false;
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef
+  ) {}
 
-  constructor() { }
-
+  @Input()
+  set ngxUnless(condition: boolean) {
+    console.log("condition", condition);
+    console.log("this.visible", this.visible);
+    if (!condition && !this.visible) {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+      this.visible = true;
+    } else if (condition && this.visible) {
+      this.viewContainer.clear();
+      this.visible = false;
+    }
+  }
 }
